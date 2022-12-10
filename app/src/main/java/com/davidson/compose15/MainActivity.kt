a4f07428afe4a80ec110120ef7c787bd6fd2bc64
Conscious-Mind <davereddragon@gmail.com>
2022-12-10 15:26:34 +0530

    Initial commit

---
+++ app/src/main/java/com/davidson/compose15/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.davidson.compose15
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.davidson.compose15.ui.theme.Compose15DailyPlannerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose15DailyPlannerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Compose15DailyPlannerTheme {
+        Greeting("Android")
+    }
+}
